1673e4eda601915a6d889ada08230f880757b5f8
Joe Bryant <terrahnahjacitor@gmail.com>
2015-04-08 22:26:37 +1000

    month stuff. getting messy

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class TransitingFile:
     def __init__ (self, file, src, dest):
         self.file = file 
         self.filename = os.path.join(src, file)
-        self.timestring = time.strftime('%Y%m%d', time.gmtime(os.path.getctime(self.filename)))
+        self.timestring = time.strftime('%Y%m%d', time.gmtime(os.path.getctime(self.filename))) # YYYYMMDD
         self.destination = os.path.join(dest, self.timestring)
         self.newfilename = os.path.join(self.destination, self.file)
         pass
@@ -26,10 +26,18 @@ class TransitingFile:
         print 'copying to:', self.newfilename
 
     def copyTo_PATH3_(self):
-        
-        pass
+        # only do jpg files
+        if self.file[-3:] != 'JPG':
+            return
+        folder = os.path.join(_PATH3_, self.timestring[:-2])
+        filename = os.path.join(folder, self.file)
+        if not (os.path.exists(folder)):
+            print 'making directory:', folder
+            os.makedirs(folder)
+        shutil.copyfile(self.newfilename, filename)
+        print 'copying to:', filename
 
 for file in os.listdir(_PATH1_):
     transitingFile = TransitingFile(file, _PATH1_,_PATH2_)
-    #transitingFile.copyTo_PATH2_()
+    transitingFile.copyTo_PATH2_()
     transitingFile.copyTo_PATH3_()
