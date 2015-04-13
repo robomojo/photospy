4817d04c4219bc8d1a521acc13b98f994394c842
Joe Bryant <terrahnahjacitor@gmail.com>
2015-04-13 23:25:03 +1000

    builds pyside

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    "packages": ["os", "time", "shutil", "datetime", "PIL"], 
+    "packages": [
+        "os", "time", "shutil", "datetime", 
+        "PIL",
+        "PySide"
+        #"PySide.QtCore.Slot", "PySide.QtCore.QMetaObject",
+        #"PySide.QtUiTools.QUiLoader", 
+        #"PySide.QtGui.QApplication", "PySide.QtGui.QMainWindow", "PySide.QtGui.QMessageBox"
+        ], 
     "include_files": ["main.py"],
-    "excludes": ["tkinter", "PySide"]
+    "excludes": ["tkinter"]
 }
 
 # GUI applications require a different base on Windows (the default is for a
