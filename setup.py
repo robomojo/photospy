44c95b41014b3af263b9068a128e706086f7138e
Joe Bryant <terrahnahjacitor@gmail.com>
2015-04-13 22:31:27 +1000

    build script

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {
+    "packages": ["os", "time", "shutil", "datetime", "PIL"], 
+    "include_files": ["main.py"],
+    "excludes": ["tkinter", "PySide"]
+}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name = "photospy",
+    version = "0.1",
+    description = "My GUI application!",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("main.py", base=base)]
+)
