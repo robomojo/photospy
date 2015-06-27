d1b47f896d042ad41672693edeba4b5ed8e7756c
Joe Bryant <terrahnahjacitor@gmail.com>
2015-06-27 23:59:12 +1000

    replacing ui with arguments file

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import os, time
 import shutil
 import datetime
 import sys
+import ConfigParser
 # PILLOW MODULES
 from PIL import Image
-# PYSIDE MODULES
-from PySide import QtGui
-from PySide import QtCore
-from PySide import QtUiTools
 
 # determine if application is ascript file or frozen exe
 if getattr(sys, 'frozen', False):
@@ -16,12 +13,13 @@ if getattr(sys, 'frozen', False):
 elif __file__:
     SCRIPT_DIRECTORY = os.path.dirname(__file__)
 
+CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, 'example.ini')
 UI_FILE = os.path.join(SCRIPT_DIRECTORY, 'photospy.ui')
 
 class TransitingFile:
     def __init__ (self, file):
-        self.file = os.path.basename(file) 
-        self.filename = file 
+        self.file = os.path.basename(file)
+        self.filename = file
         self.timestring = time.strftime('%Y%m%d', time.gmtime(os.path.getctime(self.filename))) # YYYYMMDD
     def copyTo (self, directory, pre=None):
         file = self.file if pre == None else pre + self.file
@@ -64,83 +62,44 @@ class ImageFilePair:
         self.right = key
         self.rightfile = file
 
-class UiLoader(QtUiTools.QUiLoader):
-    def __init__(self, baseinstance):
-        QtUiTools.QUiLoader.__init__(self, baseinstance)
-        self.baseinstance = baseinstance
-
-    def createWidget(self, class_name, parent=None, name=''):
-        if parent is None and self.baseinstance:
-            return self.baseinstance
-        else:
-            widget = QtUiTools.QUiLoader.createWidget(self, class_name, parent, name)
-            if self.baseinstance:
-                setattr(self.baseinstance, name, widget)
-            return widget
-
-class MainWindow(QtGui.QMainWindow):
-    def __init__(self, parent=None, **kwargs):
-        QtGui.QMainWindow.__init__(self, parent)
-        self.myWidget = self.loadUi(kwargs['uifile'], self)
-    def loadUi(self, uifile, baseinstance=None):
-        loader = UiLoader(baseinstance)
-        widget = loader.load(uifile)
-        QtCore.QMetaObject.connectSlotsByName(widget)
-        return widget
-
 class App:
-    def __init__(self):
-        self.app = QtGui.QApplication(sys.argv)
-        self.window = MainWindow(uifile=UI_FILE)
-        self.ui = self.window.myWidget
-        self.ui.start.clicked.connect(lambda: self.start())
-        self.window.show()
-    def onButtonPress(self):
-        sender = self.sender()
-        print sender.text()
-    def outputText(self, text):
-        self.ui.output.appendPlainText(text)
-        self.ui.output.repaint()
-
+    def __init__(self, args):
+        self.args = args
+        self.start()
     def start(self):
-        self.outputText('starting.')
-        _PATH1_ = self.ui.cameradir.text()
-        _PATH2_ = self.ui.harddrivedir.text()
-        _PATH3_ = self.ui.websyncdir.text()
-        self.outputText(_PATH1_)
-        self.outputText(_PATH2_)
-        self.outputText(_PATH3_)
-        if self.ui.copy_camera_to_hdd.isChecked():
-            self.outputText('starting copy from camera to hard drive')
-            for file in os.listdir(_PATH1_):
-                transitingFile = TransitingFile(os.path.join(_PATH1_, file))
-                directory = os.path.join(_PATH2_, transitingFile.timestring[:8])
-                transitingFile.copyTo(directory)
-        if self.ui.copy_hdd_to_websync.isChecked():
-            self.outputText('starting copy from hard drive to web sync')
-            for root, dirs, files in os.walk(_PATH2_):
+        print('starting.')
+        if self.args.bool_copyFromCameraToHdd:
+            print('starting copy from camera to hard drive')
+            for file in os.listdir(self.args.camera):
+                transitingFile = TransitingFile(os.path.join(self.args.camera, file))
+                directory = os.path.join(self.args.hdd, transitingFile.timestring[:8])
+                print transitingFile.copyTo(directory)
+        if self.args.bool_copyFromHddToWebsync:
+            print('starting copy from hard drive to web sync')
+            for root, dirs, files in os.walk(self.args.hdd):
                 for file in files:
                     if file[-3:] != 'JPG': # only do jpg files
                         continue
                     YYYYMM = os.path.split(root)[-1][:6] # expecting YYYYMM
                     YYYYMMDD = os.path.split(root)[-1][:8] # expecting YYYYMMDD
-                    directory = os.path.join(_PATH3_, YYYYMM) 
+                    directory = os.path.join(self.args.websync, YYYYMM)
                     if not (os.path.exists(directory)):
-                        self.outputText('main mkdir:'+directory)
+                        print('main mkdir:'+directory)
                         os.makedirs(directory)
                     newImageFilePath = os.path.join(directory, YYYYMMDD+'_'+file)
                     if not (os.path.exists(newImageFilePath)):
-                        self.outputText('copying: '+newImageFilePath)
+                        print('copying: '+newImageFilePath)
                         existingImageFilePath = os.path.join(root, file)
                         image = ImageFile(existingImageFilePath)
                         image.open()
-                        image.resize(1200)
-                        image.write(newImageFilePath)
+                        if self.args.bool_resizeWebsyncImage:
+                            print image.resize(self.args.resize)
+                        print image.write(newImageFilePath)
                         image.close()
-        if self.ui.sync_hdd_to_websync.isChecked():
-            self.outputText('starting sync hard drive to web sync')
+        if self.args.bool_syncUpdatesFromHddToWebsync:
+            print('starting sync hard drive to web sync')
             ImageFilePairs = []
-            for root, dirs, files in os.walk(_PATH2_):
+            for root, dirs, files in os.walk(self.args.hdd):
                 for file in files:
                     if file[-3:] != 'JPG': # only do jpg files
                         continue
@@ -149,11 +108,11 @@ class App:
                     ifp.left = YYYYMMDD+'_'+file
                     ifp.leftfile = os.path.join(root, file)
                     ImageFilePairs.append(ifp)
-            for root, dirs, files in os.walk(_PATH3_):
+            for root, dirs, files in os.walk(self.args.websync):
                 for file in files:
                     if file[-3:] != 'JPG': # only do jpg files
                         continue
-                    self.outputText(file)
+                    print(file)
                     match = next((x for x in ImageFilePairs if x.left == file), None)
                     if match:
                         match.right = file
@@ -161,18 +120,29 @@ class App:
             for ifp in ImageFilePairs:
                 if ifp.left and ifp.right:
                     if os.path.getmtime(ifp.leftfile) > os.path.getmtime(ifp.rightfile):
-                        self.outputText('syncing: '+ifp.rightfile)
+                        print('syncing: '+ifp.rightfile)
                         image = ImageFile(ifp.leftfile)
                         image.open()
-                        if self.ui.do_resize_for_web_sync.isChecked():
-                            image.resize(self.ui.web_sync_size.value())
-                        image.write(ifp.rightfile)
+                        if self.args.bool_resizeWebsyncImage:
+                            print image.resize(self.args.resize)
+                        print image.write(ifp.rightfile)
                         image.close()
 
+class Args:
+    def __init__(self):
+        self.config = ConfigParser.ConfigParser()
+        self.config.read(CONFIG_FILE)
+        self.camera = self.config.get('folders', 'camera')
+        self.hdd = self.config.get('folders', 'hdd')
+        self.websync = self.config.get('folders', 'websync')
+        self.bool_copyFromCameraToHdd = self.config.getboolean('jobs','copy_camera_to_hdd')
+        self.bool_copyFromHddToWebsync = self.config.getboolean('jobs','copy_hdd_to_websync')
+        self.bool_syncUpdatesFromHddToWebsync = self.config.getboolean('jobs','sync_hdd_to_websync')
+        self.bool_resizeWebsyncImage = self.config.getboolean('jobs','resize_websync')
+        self.resize = self.config.getint('options','resize')
+
 def main():
-    app = App()
-    app.outputText('Started')
-    app.app.exec_()
+    app = App(Args())
 
 if __name__ == '__main__':
     main()
