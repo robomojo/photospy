e503330a01a37f8e6eb27ae4d84194a5d8647448
Joe Bryant <terrahnahjacitor@gmail.com>
2015-06-28 00:32:33 +1000

    cleanup

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ elif __file__:
     SCRIPT_DIRECTORY = os.path.dirname(__file__)
 
 CONFIG_FILE = os.path.join(SCRIPT_DIRECTORY, 'example.ini')
-UI_FILE = os.path.join(SCRIPT_DIRECTORY, 'photospy.ui')
 
 class TransitingFile:
     def __init__ (self, file):
@@ -70,19 +69,19 @@ class App:
         print('starting.')
         if self.args.bool_copyFromCameraToHdd:
             print('starting copy from camera to hard drive')
-            for file in os.listdir(self.args.camera):
-                transitingFile = TransitingFile(os.path.join(self.args.camera, file))
-                directory = os.path.join(self.args.hdd, transitingFile.timestring[:8])
+            for file in os.listdir(self.args.directoryCamera):
+                transitingFile = TransitingFile(os.path.join(self.args.directoryCamera, file))
+                directory = os.path.join(self.args.directoryHdd, transitingFile.timestring[:8])
                 print transitingFile.copyTo(directory)
         if self.args.bool_copyFromHddToWebsync:
             print('starting copy from hard drive to web sync')
-            for root, dirs, files in os.walk(self.args.hdd):
+            for root, dirs, files in os.walk(self.args.directoryHdd):
                 for file in files:
                     if file[-3:] != 'JPG': # only do jpg files
                         continue
                     YYYYMM = os.path.split(root)[-1][:6] # expecting YYYYMM
                     YYYYMMDD = os.path.split(root)[-1][:8] # expecting YYYYMMDD
-                    directory = os.path.join(self.args.websync, YYYYMM)
+                    directory = os.path.join(self.args.directoryWebSync, YYYYMM)
                     if not (os.path.exists(directory)):
                         print('main mkdir:'+directory)
                         os.makedirs(directory)
@@ -93,13 +92,13 @@ class App:
                         image = ImageFile(existingImageFilePath)
                         image.open()
                         if self.args.bool_resizeWebsyncImage:
-                            print image.resize(self.args.resize)
+                            print image.resize(self.args.int_resize)
                         print image.write(newImageFilePath)
                         image.close()
         if self.args.bool_syncUpdatesFromHddToWebsync:
             print('starting sync hard drive to web sync')
             ImageFilePairs = []
-            for root, dirs, files in os.walk(self.args.hdd):
+            for root, dirs, files in os.walk(self.args.directoryHdd):
                 for file in files:
                     if file[-3:] != 'JPG': # only do jpg files
                         continue
@@ -108,7 +107,7 @@ class App:
                     ifp.left = YYYYMMDD+'_'+file
                     ifp.leftfile = os.path.join(root, file)
                     ImageFilePairs.append(ifp)
-            for root, dirs, files in os.walk(self.args.websync):
+            for root, dirs, files in os.walk(self.args.directoryWebSync):
                 for file in files:
                     if file[-3:] != 'JPG': # only do jpg files
                         continue
@@ -124,7 +123,7 @@ class App:
                         image = ImageFile(ifp.leftfile)
                         image.open()
                         if self.args.bool_resizeWebsyncImage:
-                            print image.resize(self.args.resize)
+                            print image.resize(self.args.int_resize)
                         print image.write(ifp.rightfile)
                         image.close()
 
@@ -132,14 +131,14 @@ class Args:
     def __init__(self):
         self.config = ConfigParser.ConfigParser()
         self.config.read(CONFIG_FILE)
-        self.camera = self.config.get('folders', 'camera')
-        self.hdd = self.config.get('folders', 'hdd')
-        self.websync = self.config.get('folders', 'websync')
+        self.directoryCamera = self.config.get('folders', 'camera')
+        self.directoryHdd = self.config.get('folders', 'hdd')
+        self.directoryWebSync = self.config.get('folders', 'websync')
         self.bool_copyFromCameraToHdd = self.config.getboolean('jobs','copy_camera_to_hdd')
         self.bool_copyFromHddToWebsync = self.config.getboolean('jobs','copy_hdd_to_websync')
         self.bool_syncUpdatesFromHddToWebsync = self.config.getboolean('jobs','sync_hdd_to_websync')
         self.bool_resizeWebsyncImage = self.config.getboolean('jobs','resize_websync')
-        self.resize = self.config.getint('options','resize')
+        self.int_resize = self.config.getint('options','resize')
 
 def main():
     app = App(Args())
